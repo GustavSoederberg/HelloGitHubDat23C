e8f476f33a948fbb42248e6fa88615d5d73ff9eb
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-08 15:00:48 +0200

    added message Hva så

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hva såååååååå");
         System.out.println("Hej Kea");
         System.out.println("Hej igen igen");
+        System.out.println("hva så");
     }
 }
