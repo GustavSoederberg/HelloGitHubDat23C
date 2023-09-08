5cec105a29ff7fc82649f43f127323c1dff460b5
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-08 14:23:28 +0200

    added Hello Kea message

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hva såååååååå");
-        System.out.println("Hej hej");
-        System.out.println("Hello KEA");
+        System.out.println("Hej Kea");
     }
 }
