42edb4a606ab7898a3abb5df1f977f7db61db0a6
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-08 14:56:29 +0200

    added message igen igen

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hva såååååååå");
         System.out.println("Hej Kea");
+        System.out.println("Hej igen igen");
     }
 }
